561e4df22031762bc79c1e1d5c055b752282edee
DarthJenow <darthjenow@gmail.com>
2015-12-08 23:33:00 +0100

    v0.1

---
+++ Calc_Train/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Calc_Train
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxResult = new System.Windows.Forms.TextBox();
+            this.labelTask = new System.Windows.Forms.Label();
+            this.buttonEnterResult = new System.Windows.Forms.Button();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.eventLog1 = new System.Diagnostics.EventLog();
+            this.richTextBoxLog = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxResult
+            // 
+            this.textBoxResult.Location = new System.Drawing.Point(96, 25);
+            this.textBoxResult.Name = "textBoxResult";
+            this.textBoxResult.Size = new System.Drawing.Size(75, 20);
+            this.textBoxResult.TabIndex = 0;
+            this.textBoxResult.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxResult_KeyPress);
+            // 
+            // labelTask
+            // 
+            this.labelTask.AutoSize = true;
+            this.labelTask.Location = new System.Drawing.Point(95, 9);
+            this.labelTask.Name = "labelTask";
+            this.labelTask.Size = new System.Drawing.Size(53, 13);
+            this.labelTask.TabIndex = 1;
+            this.labelTask.Text = "Operation";
+            // 
+            // buttonEnterResult
+            // 
+            this.buttonEnterResult.Location = new System.Drawing.Point(177, 23);
+            this.buttonEnterResult.Name = "buttonEnterResult";
+            this.buttonEnterResult.Size = new System.Drawing.Size(78, 23);
+            this.buttonEnterResult.TabIndex = 2;
+            this.buttonEnterResult.Text = "Enter";
+            this.buttonEnterResult.UseVisualStyleBackColor = true;
+            this.buttonEnterResult.Click += new System.EventHandler(this.buttonEnterResult_Click);
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(12, 10);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 36);
+            this.buttonStart.TabIndex = 3;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // eventLog1
+            // 
+            this.eventLog1.SynchronizingObject = this;
+            // 
+            // richTextBoxLog
+            // 
+            this.richTextBoxLog.Location = new System.Drawing.Point(12, 52);
+            this.richTextBoxLog.Name = "richTextBoxLog";
+            this.richTextBoxLog.ReadOnly = true;
+            this.richTextBoxLog.Size = new System.Drawing.Size(243, 110);
+            this.richTextBoxLog.TabIndex = 4;
+            this.richTextBoxLog.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(267, 174);
+            this.Controls.Add(this.richTextBoxLog);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.buttonEnterResult);
+            this.Controls.Add(this.labelTask);
+            this.Controls.Add(this.textBoxResult);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.TextBox textBoxResult;
+        public System.Windows.Forms.Label labelTask;
+        public System.Windows.Forms.Button buttonEnterResult;
+        public System.Windows.Forms.Button buttonStart;
+        private System.Diagnostics.EventLog eventLog1;
+        private System.Windows.Forms.RichTextBox richTextBoxLog;
+    }
+}
+
---
+++ Calc_Train/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Calc_Train
+{
+    public partial class Form1 : Form
+    {
+        public int x1;
+        public int x2;
+        public int y;
+        public string task;
+        public Boolean solving = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            textBoxResult.Enabled = false;
+            buttonEnterResult.Enabled = false;
+            buttonStart.Text = "Start";
+        }
+
+        public void changeStart()
+        {
+            if (solving)
+            {
+                textBoxResult.Enabled = false;
+                buttonEnterResult.Enabled = false;
+                buttonStart.Text = "Start";
+
+                solving = false;
+            }
+            else
+            {
+                textBoxResult.Enabled = true;
+                buttonEnterResult.Enabled = true;
+                buttonStart.Text = "Strop";
+
+                solving = true;
+            }
+        }
+
+        public void makeTask()
+        {
+            int operation = (new Random()).Next(0, 4);
+
+            switch (operation)
+            {
+                case 0:
+                    operationPlus();
+                    break;
+                case 1:
+                    operationMinus();
+                    break;
+                case 2:
+                    operationMultiply();
+                    break;
+                case 3:
+                    operationDivide();
+                    break;
+            }
+        }
+
+        public void operationPlus()
+        {
+            Random random = new Random();
+
+            x1 = random.Next(0, 21);
+            x2 = random.Next(0, 21);
+            y = x1 + x2;
+
+            task = x1 + " + " + x2;
+
+            labelTask.Text = task;
+        }
+
+        public void operationMinus()
+        {
+            Random random = new Random();
+
+            x1 = random.Next(0, 21);
+            x2 = random.Next(0, x1);
+            y = x1 - x2;
+
+            task = x1 + " - " + x2;
+            labelTask.Text = task;
+        }
+
+        public void operationMultiply()
+        {
+            Random random = new Random();
+
+            x1 = random.Next(0, 12);
+            x2 = random.Next(0, 12);
+            y = x1 * x2;
+
+            task = x1 + " * " + x2;
+
+            labelTask.Text = task;
+        }
+
+        public void operationDivide()
+        {
+            Random random = new Random();
+
+            y = random.Next(0, 12);
+            x2 = random.Next(1, 12);
+            x1 = x2 * y;
+
+            task = x1 + " / " + x2;
+            labelTask.Text = task;
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            changeStart();
+
+            if (solving)
+            {
+                makeTask();
+            }
+            else
+            {
+                labelTask.Text = "Operation";
+            }
+        }
+
+        private void textBoxResult_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char) ConsoleKey.Enter)
+            {
+                buttonEnterResult_Click(null, null);
+
+                e.Handled = true;
+            }
+}
+
+        private void buttonEnterResult_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (Int32.Parse(textBoxResult.Text) == y)
+                {
+                    richTextBoxLog.AppendText("\u2714" + " " + task + "\r\n");
+                    textBoxResult.Text = "";
+                    makeTask();
+                }
+                else
+                {
+                    richTextBoxLog.AppendText("\u2718" + " " + task + "\r\n");
+                    textBoxResult.Text = "";
+                    labelTask.Text = task;
+                }
+            }
+            catch (FormatException)
+            {
+                richTextBoxLog.AppendText("UnexpectedInput.Exception" + "\r\n");
+                textBoxResult.Text = "";
+                labelTask.Text = task;
+            }
+
+        }
+    }
+}
